d654c0fe25bb35b3e0964df7615ba6a32d902311
Vasilii Sevostianov <sevostianov.vn@phystech.edu>
2023-06-01 17:15:01 +0200

    fix yolo - model changes

--- pages/YOLO.py
+++ pages/YOLO.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
     # Load the pre-trained model
     
     path_to_weights = 'https://github.com/vasevooo/cv_project/blob/main/yolo_model/best.pt'
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', path=path_to_weights, force_reload=True)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
 
     main()
