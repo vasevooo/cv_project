7418238ee8f89ace7a774d6489d519065af3e6aa
Vasilii Sevostianov <sevostianov.vn@phystech.edu>
2023-06-01 09:30:17 +0200

    start of the project

---
+++ main_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# Computer Vision Project by FasterRCNN 🎈")
+
+st.text ('Team members: \n 1. Vasily S. \n 2. Anna F. \n 3. Viktoria K. \n 4. Maria K. \n 5. Ilvir Kh.')
