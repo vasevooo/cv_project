0dea7d859119873bc1495930568c4959b952c39a
MARI-posa <128982101+MARI-posa@users.noreply.github.com>
2023-06-02 16:14:24 +0300

    Update YOLO.py

--- pages/YOLO.py
+++ pages/YOLO.py
@@ -70,39 +70,40 @@ col1, col2, col3 = st.columns([2,5,2])
 with col2:
     
     def main():
+    
+    st.text("Upload an image and the Detector will determine \nif a tumor is positive or negative")
 
-         st.text("Upload an image and the Detector will determine \nif a tumor is positive or negative")
-       
+    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-        uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    if uploaded_image is not None:
+        # Read the image file
+        image = preprocess_image(Image.open(uploaded_image))
+        results = model(image)
+        
+        # Plot the uploaded image and results side by side
+        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+        axs[0].imshow(image, cmap = 'gray')
+        axs[0].axis("off")
+        axs[0].set_title("Uploaded Image")
+        axs[1].imshow(results.render()[0])
+        axs[1].axis("off")
+        axs[1].set_title("Detection Result")
+        st.pyplot(fig)
 
-        if uploaded_image is not None:
-            # Read the image file
-            image = preprocess_image(Image.open(uploaded_image))
-            results = model(image)
-
-            # Plot the uploaded image and results side by side
-            fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-            axs[0].imshow(image, cmap = 'gray')
-            axs[0].axis("off")
-            axs[0].set_title("Uploaded Image")
-            axs[1].imshow(results.render()[0])
-            axs[1].axis("off")
-            axs[1].set_title("Detection Result")
-            st.pyplot(fig)
-
-    def preprocess_image(image):
-        transforms_pipeline = transforms.Compose([
-            transforms.Resize((640, 640))
-
-        ])
-        return transforms_pipeline(image)
+def preprocess_image(image):
+    transforms_pipeline = transforms.Compose([
+        transforms.Resize((640, 640))
+        
+    ])
+    return transforms_pipeline(image)
 
 
-    if __name__ == "__main__":
-        # Load the pre-trained model
+if __name__ == "__main__":
+    # Load the pre-trained model
+    
+    path_to_weights = 'yolo_model/best.pt'
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
 
-        path_to_weights = 'yolo_model/best.pt'
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
+    main()
 
         main()
