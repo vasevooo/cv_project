19b5b92f306e661a23190ee1a4c19e341f075082
MARI-posa <128982101+MARI-posa@users.noreply.github.com>
2023-06-02 16:59:02 +0300

    Update f_1.py

--- pages/f_1.py
+++ pages/f_1.py
@@ -59,44 +59,48 @@ import torch
 import matplotlib.pyplot as plt
 from torchvision import transforms
 
+col1, col2, col3 = st.columns([1,8,1])
+#col1, col2 = st.columns(2)
 
-st.markdown("## F1 car detection")
+### Гистограмма total_bill
+with col2:
+    st.markdown("## F1 car detection")
 st.sidebar.success("You are currently viewing F1 car detection page")
 
+with col2:
+    def main():
 
-def main():
-    
-    st.text("Upload an image and the Detector will determine \n4 models of the F1 cars: \n Mercedes, RedBull, McLaren, Ferrari")
+        st.text("Upload an image and the Detector will determine \n4 models of the F1 cars: \n Mercedes, RedBull, McLaren, Ferrari")
 
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+        uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    if uploaded_image is not None:
-        # Read the image file
-        image = preprocess_image(Image.open(uploaded_image))
-        results = model(image)
-        
-        # Plot the uploaded image and results side by side
-        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-        axs[0].imshow(image, cmap = 'gray')
-        axs[0].axis("off")
-        axs[0].set_title("Uploaded Image")
-        axs[1].imshow(results.render()[0])
-        axs[1].axis("off")
-        axs[1].set_title("Detection Result")
-        st.pyplot(fig)
+        if uploaded_image is not None:
+            # Read the image file
+            image = preprocess_image(Image.open(uploaded_image))
+            results = model(image)
 
-def preprocess_image(image):
-    transforms_pipeline = transforms.Compose([
-        transforms.Resize((640, 640))
-        
-    ])
-    return transforms_pipeline(image)
+            # Plot the uploaded image and results side by side
+            fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+            axs[0].imshow(image, cmap = 'gray')
+            axs[0].axis("off")
+            axs[0].set_title("Uploaded Image")
+            axs[1].imshow(results.render()[0])
+            axs[1].axis("off")
+            axs[1].set_title("Detection Result")
+            st.pyplot(fig)
+
+    def preprocess_image(image):
+        transforms_pipeline = transforms.Compose([
+            transforms.Resize((640, 640))
+
+        ])
+        return transforms_pipeline(image)
 
 
-if __name__ == "__main__":
-    # Load the pre-trained model
-    
-    path_to_weights = 'yolo_model/best_f1_50e.pt'
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
+    if __name__ == "__main__":
+        # Load the pre-trained model
 
-    main()
+        path_to_weights = 'yolo_model/best_f1_50e.pt'
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
+
+        main()
