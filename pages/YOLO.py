9a522e2aa314c9af590ab207ec845004954d7fcd
MARI-posa <128982101+MARI-posa@users.noreply.github.com>
2023-06-02 16:34:17 +0300

    Update YOLO.py

--- pages/YOLO.py
+++ pages/YOLO.py
@@ -1,56 +1,4 @@
-import streamlit as st
-# with open('style.css') as f:
-#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-import base64
-import streamlit as st
-import plotly.express as px
-
-df = px.data.iris()
-
-@st.cache_data
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.ibb.co/T2xQ5hG/imgonline-com-ua-Black-White-KX3-XXY2dh-Yh2-Xa0.jpg");
-background-size: 110%;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("https://i.ibb.co/n75r0q1/angryimg.png");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-
-[data-testid="stHeader"] {{
-background: rgba(1,1,1,1);
-}}
-
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-
-div.css-1n76uvr.e1tzin5v0 {{
-background-color: rgba(238, 238, 238, 0.5);
-border: 10px solid #EEEEEE;
-padding: 5% 5% 5% 10%;
-border-radius: 5px;
-}}
-
-
-
-</style>
-"""
-import streamlit as st
+    import streamlit as st
 from PIL import Image
 import torch
 import matplotlib.pyplot as plt
@@ -97,4 +45,3 @@ if __name__ == "__main__":
     model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to_weights, force_reload=True)
 
     main()
-
