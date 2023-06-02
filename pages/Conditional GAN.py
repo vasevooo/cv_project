94aed6453a65d43ecee6b710fc260677e72c2ba8
MARI-posa <128982101+MARI-posa@users.noreply.github.com>
2023-06-02 17:06:15 +0300

    Update Conditional GAN.py

--- pages/Conditional GAN.py
+++ pages/Conditional GAN.py
@@ -40,7 +40,7 @@ background: rgba(0,0,0,0);
 right: 2rem;
 }}
 
-div.css-1n76uvr.e1tzin5v0 {{
+div.css-1n76uvr.esravye0 {{
 background-color: rgba(238, 238, 238, 0.5);
 border: 10px solid #EEEEEE;
 padding: 5% 5% 5% 10%;
