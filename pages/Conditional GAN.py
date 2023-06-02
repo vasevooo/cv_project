2d84c5d9ef90d0f0435b74b4bad4d12815b4cf70
MARI-posa <128982101+MARI-posa@users.noreply.github.com>
2023-06-02 15:58:52 +0300

    Update Conditional GAN.py

--- pages/Conditional GAN.py
+++ pages/Conditional GAN.py
@@ -1,4 +1,7 @@
 import streamlit as st
+# with open('style.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 import base64
 import streamlit as st
 import plotly.express as px
@@ -11,22 +14,19 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-
-#img = get_img_as_base64("https://catherineasquithgallery.com/uploads/posts/2021-02/1612739741_65-p-goluboi-fon-tsifri-110.jpg")
-
+css-1n76uvr e1tzin5v0
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.ibb.co/rQ0pTTx/imgonline-com-ua-Black-White-KEZW2w9mmw67c.jpg");
-background-size: 115%;
+background-image: url("https://avatars.mds.yandex.net/i?id=0af4b340256b74ab0c94fc3f60639c26e8b58124-9093415-images-thumbs&n=13");
+background-size: 130%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
 
 [data-testid="stSidebar"] > div:first-child {{
-background-image: url("https://ibb.co/ZBkdJRg");
-background-size: 115%;
+background-image: url("https://klike.net/uploads/posts/2022-11/1668930175_8-3.jpg");
 background-position: center; 
 background-repeat: no-repeat;
 background-attachment: fixed;
@@ -38,6 +38,13 @@ background: rgba(0,0,0,0);
 
 [data-testid="stToolbar"] {{
 right: 2rem;
+}}
+
+div.css-1n76uvr.e1tzin5v0 {{
+background-color: rgba(238, 238, 238, 0.5);
+border: 10px solid #EEEEEE;
+padding: 5% 5% 5% 10%;
+border-radius: 5px;
 }}
 </style>
 """
@@ -66,15 +73,23 @@ def generate_image(number):
     generated_image = tf.image.resize(generated_image[None, ...], (28, 28))[0]  # Добавлено [None, ...] для добавления измерения
     return generated_image
 
-# Веб-приложение с использованием Streamlit
-st.title('Генерация изображений с Conditional GAN')
-number = st.slider('Выберите число:', 0, 9, step=1)
-
-col1, col2, col3 = st.columns([2,4,2])
+col1, col2, col3 = st.columns([1,8,1])
 #col1, col2 = st.columns(2)
 
 ### Гистограмма total_bill
 with col2:
+# Веб-приложение с использованием Streamlit
+    st.title('Генерация изображений с Conditional GAN')
+col1, col2, col3 = st.columns([2,5,2])
+#col1, col2 = st.columns(2)
+
+### Гистограмма total_bill
+with col2:
+# Веб-приложение с использованием Streamlit
+    
+    number = st.slider('Выберите число:', 0, 9, step=1)
+
+
  #col1.subheader("Гистограмма total_bill:")
 
     # Генерация и отображение изображения
